3550ff0799e7c1a9f989ad602374e1fa4dcdcdec
unknown <simon@ishere.help>
2025-12-03 21:48:46 -0800

    nextAuth 5b: typo

--- examples/with-nextjs/app/SessionProvider.tsx
+++ examples/with-nextjs/app/SessionProvider.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import type React from "react";
 
-export function NextAuthSessionProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function NextAuthSessionProvider(props: { children: any }) {
+  return <SessionProvider>{props.children}</SessionProvider>;
 }
--- examples/with-nextjs/app/layout.tsx
+++ examples/with-nextjs/app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NextAuthSessionProvider>
-          {children}
-        </NextAuthSessionProvider>
+        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
       </body>
     </html>
   );
